4319792854706f03c22f3a9de1222103ad904262
Andrew <andreychopko@gmail.com>
2016-05-19 13:58:55 +0300

    Added show/create/delete actions

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,19 +1,52 @@
 class HousesController < ApplicationController
+
+  before_action :find_house, only: [:show, :edit, :destroy, :update]
+
   def index
+    @houses = House.all
   end
 
   def new
+    @house = House.new
+  end
+
+  def show
   end
 
   def create
+    @house = House.new(house_params)
+
+    if @house.save
+      redirect_to @house
+    else
+      render 'New'
+    end
   end
 
   def destroy
+    @house.destroy
+    redirect_to root_path
   end
 
   def update
+    if @house.update(house_params)
+      redirect_to @house
+    else
+      render "Edit"
+    end
   end
 
   def edit
+
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:title, :description)
+  end
+
+  def find_house
+    @house = House.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'houses/index'
 
-  get 'houses/new'
-
-  get 'houses/create'
-
-  get 'houses/destroy'
-
-  get 'houses/update'
-
-  get 'houses/edit'
-
+  resources :houses
   root "houses#index"
 end
